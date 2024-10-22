3c7561a41a95cbb0f911e202951843648fb5831d
rodrigocarvalho10 <rodrigo.carvalho.it@gmail.com>
2024-10-22 15:42:05 -0300

    Subindo versão 0.1.1

--- tabuada.py
+++ tabuada.py
@@ -1,16 +1,15 @@
 """
 Imprimea a tabuada do 1 ao 10
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Rodrigo"
 
 # Definicao dos numeros a serem calculados
 numeros = list(range(1, 11))
 
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        resultado = numero * outro_numero
-        #print(str(numero) + " x " + str(outro_numero) + " = " + str(resultado))
-        print(f"O valor de {numero} x {outro_numero} e {resultado}")
-    print("-" * 20)
+for n1 in numeros:
+    print("{:-^20}".format(f"Tabuado do {n1}"))
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 20)

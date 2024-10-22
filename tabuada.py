9b207ff3087d603a3dc5f6b808d3347b5cc5e18a
rodrigocarvalho10 <rodrigo.carvalho.it@gmail.com>
2024-10-22 11:14:58 -0300

    Subindo a tabuada

--- tabuada.py
+++ tabuada.py
@@ -11,5 +11,6 @@ for numero in numeros:
     print("Tabuada do:", numero)
     for outro_numero in numeros:
         resultado = numero * outro_numero
-        print(str(numero) + " x " + str(outro_numero) + " = " + str(resultado))
+        #print(str(numero) + " x " + str(outro_numero) + " = " + str(resultado))
+        print(f"O valor de {numero} x {outro_numero} e {resultado}")
     print("-" * 20)
